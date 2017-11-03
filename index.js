26420308cde6f49379250624f0c313c13c2a24d1
Ian Pavlovic <ian_pavlovic@msn.com>
2017-11-03 18:05:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function deleteFromObjectByKey(object, key){
-  return Object.assign({}, delete object[key])
+  var newDelete = Object.assign({}, delete object[key])
+  return newDelete
 }
